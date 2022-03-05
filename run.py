56a5bf86b8b1f7c4b049c79b1e171bec83cf0a52
ellouisefm <85605042+ellouisefm@users.noreply.github.com>
2022-03-05 22:35:00 +0800

    update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from server import server
-from CaseStudyDash import app as CaseStudyDash
+from dashapp import CaseStudyDash as app
+# from CaseStudyDash import app as CaseStudyDash
 
 if __name__ == '__main__':
     server.run()
