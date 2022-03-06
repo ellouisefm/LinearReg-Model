87f580b96f02a7475e8fc863eb69af7565ad5973
ellouisefm <85605042+ellouisefm@users.noreply.github.com>
2022-03-06 18:08:20 +0800

    update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from server import server
-from dashapp import CaseStudyDash as CSDash
+from dashapp.CaseStudyDash import app
 # from CSDash import app as cs_dash
 
 if __name__ == '__main__':
     server.run(debug=False)
+    app.run_server(debug=False)
